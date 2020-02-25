233966fef3a01c457beec93028de3283f20b5c4f
ernegreiros <edvandro.rodrigues@agilecontent.com>
2020-02-25 16:51:39 -0300

    Design Patterns Course - Strategy

--- DesignPatterns/Strategy/CalculadorDeImposto.cs
+++ DesignPatterns/Strategy/CalculadorDeImposto.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Strategy
+namespace Strategy_Imposto
 {
     public class CalculadorDeImposto
     {
--- DesignPatterns/Strategy/Program.cs
+++ DesignPatterns/Strategy/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Strategy
+namespace Strategy_Imposto
 {
     class Program
     {
